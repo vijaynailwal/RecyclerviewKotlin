fc92aa3cfbd3791f459b1895cdf6061b5c9255c1
vijay <vjynlwl93@gmail.com>
2020-05-04 16:47:40 +0530

    spacing(topitemdecortion,glide(requestoption))

---
+++ app/src/main/java/com/example/recyclerviewkotlin/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.recyclerviewkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var blogAdapter: BlogRecyclerAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initRecyclerView()
+        addDataSet()
+    }
+    private fun addDataSet(){
+        val data = DataSource.createDataSet()
+        blogAdapter.submitList(data)
+    }
+    private fun initRecyclerView(){
+        recycler_view.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            val topSpacingDecorator = TopSpacingItemDecoration(30)
+            addItemDecoration(topSpacingDecorator)
+            blogAdapter = BlogRecyclerAdapter()
+            adapter = blogAdapter
+        }
+    }
+}
---
+++ app/src/main/java/com/example/recyclerviewkotlin/models/BlogPost.kt
@@ -0,0 +1,12 @@
+package com.example.recyclerviewkotlin.models
+
+data class BlogPost(
+        var title: String,
+        var body: String,
+        var image: String,
+        var username: String // Author of blog post
+) {
+    override fun toString(): String {
+        return "BlogPost(title='$title', image='$image', username='$username')"
+    }
+}
